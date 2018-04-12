45ab34c9d0694ffb4ffce9d878e1594d37854a32
Robert Zacharias <bobby.zacharias@gmail.com>
2018-04-12 15:53:45 -0400

    pulling RTS pin low

--- sign_serial_print_diagnostic/sign_serial_print_diagnostic.ino
+++ sign_serial_print_diagnostic/sign_serial_print_diagnostic.ino
@@ -5,9 +5,13 @@ over Serial2 from the podium
 4-12-18 rz@rzach.me
 */
 
+const int RTSpin = 5;
+
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
+  pinMode(RTSpin, OUTPUT);
+  digitalWrite(RTSpin, LOW);
 }
 
 void loop() {
