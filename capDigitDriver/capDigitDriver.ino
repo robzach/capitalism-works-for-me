83e7f96db8b0e8cd09846dd7d747246beac8d287
Robert Zacharias <bobby.zacharias@gmail.com>
2018-05-24 21:31:19 -0400

    successfully reading in serial data via RS485 and printing last three digits of it to 7-segment display

--- capDigitDriver/capDigitDriver.ino
+++ capDigitDriver/capDigitDriver.ino
@@ -15,26 +15,42 @@ AltSoftSerial altSerial;
 // tie this pin to gnd to mark this as the "FALSE" side, or
 // leave this pin floating to mark this as the "TRUE" side
 const int SIDEPIN = A5;
-bool TRUESIDE; // this bool will be set by wiring in the setup()
+bool trueside; // this bool will be set in the setup() by physical wiring
 
 // SPI pins
-const int SCK = 13;
-const int MISO = 12;
-const int MOSI = 11;
+const int SCKpin = 13;
+const int MISOpin = 12;
+const int MOSIpin = 11;
+const int GND = A2; // using pin A2 as a logic ground since otherwise none is broken out
 
 const int RS485RECEIVEENABLEPIN = 7; // RS485 transceiver: pull this pin low to receive
+
+const unsigned long WAIT = 1000; // millisecond wait between instances of pushing SPI data
+unsigned long timer;
 
 void setup() {
   Serial.begin(9600); // hardware serial for local USB debugging
   altSerial.begin(9600); // software serial for receiving remote signal
 
+  // this pin, configured in hardware, tells the board whether it's the TRUE or FALSE side
   pinMode(SIDEPIN, INPUT_PULLUP);
-  TRUESIDE = digitalRead(SIDEPIN);
+  trueside = digitalRead(SIDEPIN);
 
+  // required for receiving data on RS485
   pinMode(RS485RECEIVEENABLEPIN, OUTPUT);
   digitalWrite(RS485RECEIVEENABLEPIN, LOW);
 
-  // set up pin modes for SPI(?) output
+  // SPI communication
+  pinMode(SCKpin, OUTPUT);
+  pinMode(MISOpin, OUTPUT);
+  pinMode(MOSIpin, OUTPUT);
+  pinMode(GND, OUTPUT);
+  digitalWrite(GND, LOW);
+
+  //Blank out the register
+  shiftOut(MOSIpin, SCKpin, MSBFIRST, 0x00);
+  digitalWrite(MISOpin, HIGH);
+  digitalWrite(MISOpin, LOW);
 }
 
 void loop() {
@@ -43,12 +59,12 @@ void loop() {
   // listen for incoming instructions
   // format: "t123\nf456\n" means the true count is 123 and the false count is 456
   if (altSerial.available() > 0) {
-    if (TRUESIDE && altSerial.peek() == 't') {
+    if (trueside && altSerial.peek() == 't') {
       inVal = altSerial.parseInt();
       Serial.print("true value received: ");
       Serial.println(inVal);
     }
-    else if (!TRUESIDE && altSerial.peek() == 'f') {
+    else if (!trueside && altSerial.peek() == 'f') {
       inVal = altSerial.parseInt();
       Serial.print("false value received: ");
       Serial.println(inVal);
@@ -57,10 +73,35 @@ void loop() {
   }
 
   // print the value on the local 7-segment display
+  //  if (millis() - timer > WAIT) ledDisplay(inVal);
   ledDisplay(inVal);
 }
 
 void ledDisplay(int num) {
-  // write this function!
+  int ones = num % 10;
+  int tens = (num % 100) / 10;
+  int hundreds = (num % 1000) / 100;
+
+  shiftOut(MOSIpin, SCKpin, MSBFIRST, segLookup(ones));
+  shiftOut(MOSIpin, SCKpin, MSBFIRST, segLookup(tens));
+  shiftOut(MOSIpin, SCKpin, MSBFIRST, segLookup(hundreds));
+
+  digitalWrite(MISOpin, HIGH);
+  digitalWrite(MISOpin, LOW);
+
+  timer = millis();
 }
 
+byte segLookup(int digitIn) {
+  // turn a numeral into a seven-segment pattern
+  if (digitIn == 0) return 0x3f;
+  if (digitIn == 1) return 0x06;
+  if (digitIn == 2) return 0x5b;
+  if (digitIn == 3) return 0x4f;
+  if (digitIn == 4) return 0x66;
+  if (digitIn == 5) return 0x6d;
+  if (digitIn == 6) return 0x7d;
+  if (digitIn == 7) return 0x07;
+  if (digitIn == 8) return 0x7f;
+  if (digitIn == 9) return 0x6f;
+}
